3cd66c0a4a4f131310bf48a869d359e2691c24f8
Chelsea Ehimare <chelseaehimare01@gmail.com>
2023-02-05 16:26:11 +0000

    server port

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.put('/collections/:collectionName/:id', function (req, res, next) {
 });
 
 
-
-app.listen(3000, () => {
-    console.log("Server is running on 3000!")
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Server is running on port: " + port)
 });
